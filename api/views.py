51f0149a74990630373688440c979d89e0a85e57
Khalil-Ullah-Hafeez <khalil.formal@gmail.com>
2023-04-15 14:44:13 +0500

    ADDED - csrf exempt

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 
+from django.views.decorators.csrf import csrf_exempt
+
 def index(request):
     return HttpResponse("Hello, world. You're at the Nitro Backend index.")
 
+@csrf_exempt
 def handler(request):
 
     # only allow the HTTP POST method
