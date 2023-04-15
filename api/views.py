e4d9d1a98edf721592716b4767889c8aeca4a939
Khalil-Ullah-Hafeez <khalil.formal@gmail.com>
2023-04-15 14:42:27 +0500

    Updated handler view

--- api/views.py
+++ api/views.py
@@ -4,4 +4,22 @@ def index(request):
     return HttpResponse("Hello, world. You're at the Nitro Backend index.")
 
 def handler(request):
-    return HttpResponse("This is the handler")
+
+    # only allow the HTTP POST method
+    if request.method != 'POST':
+         return HttpResponse("This endpoint only supports POST request.")
+
+    # check if there are any files
+    if request.FILES.get('photo', True) == True:
+        return HttpResponse("This endpoint accepts multipart/form-data content, with field name 'photo'")
+
+
+    # # Save Image
+    # image = models.Image(photo=request.FILES.get('photo'), processed_photo=request.FILES.get('photo'))
+    # image.detectshadow()
+    # result = image.compute()
+
+    # Return response
+    return HttpResponse("End of the view")
+
+   
